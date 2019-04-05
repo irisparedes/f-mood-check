0ffceac720e9650691056afa3a0ef5ef2575aa2d
Nasiba Carrasco <nasivuela@gmail.com>
2019-04-05 20:14:22 +0200

    Add mood check

---
+++ main.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// La app tiene un select con dos caras y un botón de actualizar. Por defecto es feliz y amarillo. Al pulsar el actualizar se muestra la cara seleccionada y cambia el fondo en base a un número aleatorio: amarillo si par, naranja si impar.
+
+// Recoger el select
+const selectEl = document.querySelector('.mood__selector');
+// Recoger el boton
+const buttonEl = document.querySelector('.mood__update');
+// Recoger el título
+const titleEl = document.querySelector('.mood__face');
+// Recoger el body
+const body = document.body;
+
+// TODO: volver y intentar descifrar como funciona esto
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+// En este punto compruebo
+// console.log(selectEl, buttonEl, titleEl, body);
+
+// Escuchar click en botón
+buttonEl.addEventListener('click', handleButtonClick);
+// Crear handler de click en botón
+function handleButtonClick(event) {
+  event.preventDefault();
+  
+  // En este punto compruebo
+  //console.log('CLICK');
+  
+  // Recoger el valor del select y ponerlo en el título
+  titleEl.innerHTML =  selectEl.value;
+
+  // Generar un número aleatorio entre 1 y 100
+  const randomNumber = getRandomIntInclusive(1, 100);
+
+  // En este punto compruebo
+  // console.log(randomNumber);
+  
+  if(randomNumber%2 === 0) {
+  // Si par
+    // Quitar clase naranja de body
+    body.classList.remove('chilly-orange');
+  } else {
+  // sino
+    // Añadir clase naranja a body
+    body.classList.add('chilly-orange');
+  }
+}
